ce54fd6d0aad4a2ad6a409446b5641fe18d505d9
pleasewhy <1943788269@qq.com>
2021-12-28 20:41:18 +0800

    complete load operation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use elf;
+use elf::types::PT_LOAD;
 use riscv_decode::{self, Instruction};
 use std::env;
+use std::os::unix::prelude::FileExt;
 use std::path::PathBuf;
 
 #[macro_use]
@@ -19,10 +21,35 @@ fn main() {
         Ok(f) => f,
         Err(e) => panic!("Error: {:?}", e),
     };
-    println!("{:?}", file);
-    let text_scn = match file.get_section(".text") {
-        Some(s) => s,
-        None => panic!("Failed to look up .text section"),
-    };
-    // println!("{:?}", text_scn.data);
+    println!("entry=0x{:x}", file.ehdr.entry);
+    let mut mem = riscv_vm::memory::Memory::new();
+    let mut io_file = std::fs::File::open(path).unwrap();
+    io_file.read
+    for section in file.sections {
+        if section.shdr.addr != 0 {
+            println!(
+                "write mem: name={} addr=0x{:x}, end=0x{:x}, size=0x{:x} data.size=0x{:x}",
+                section.shdr.name,
+                section.shdr.addr,
+                section.shdr.addr + section.shdr.size,
+                section.shdr.size,
+                section.data.len()
+            );
+            mem.write_by_iter(section.shdr.addr as usize, section.data);
+        }
+    }
+    for ph in file.phdrs {
+        println!(
+            "phdr: vaddr=0x{:x} end=0x{:x} filesz=0x{:x} memsize=0x{:x}",
+            ph.vaddr,
+            ph.vaddr + ph.memsz,
+            ph.filesz,
+            ph.memsz
+        );
+    }
+
+    let mut pc = file.ehdr.entry as usize;
+    for i in 0..10000 {
+        pc += 4;
+    }
 }
